f0564f3219b714120d388b4c55c7cefcaa31d7f9
Jeremy Arimado <jarimado@gmail.com>
2016-07-10 14:02:25 +1000

    refactored: getting databse reference

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const db        = require('./db.js')
 
 const DB_URL = 'mongodb://localhost:27017/d1';
 
+const getCollection = (db, collection) => {
+    return db.get().collection(collection);
+}
+
+const connectionsDB = (db) => {
+    return getCollection(db, 'connections');
+}
+
 db.connect(DB_URL, (err) => {
     if (err) {
         console.log('could not connect to DB');
@@ -20,14 +28,21 @@ db.connect(DB_URL, (err) => {
     }
 });
 
-app.get('/', (req, res) => {
-    let connectionsDB = db.get().collection('connections');
-    connectionsDB.insert({ message: "A user connected" }, (err) => {
-        if (err) console.log('something went wrong adding to the db');
-        console.log('a user connected');
-    })
-    res.send("You've reached the server for d1");
+app.get('/',
+    (req, res) => {
+        connectionsDB(db)
+            .insert({ message: "A user connected" }, (err) => {
+            if (err) console.log('something went wrong adding to the db');
+            console.log('a user connected');
+        })
+        res.send("You've reached the server for d1");
 });
+
+app.post('/state',
+    (req, res) => {
+        let state = JSON.parse(req.body.data);
+        let stateDB = db.get
+})
 
 http.listen(process.env.PORT || 3005, function () {
     let port = process.env.PORT || 3005;
