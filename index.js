e75e44705625742f59a9c88cf7c4b70df8677437
Jeremy Arimado <jarimado@gmail.com>
2016-07-14 13:53:38 +1000

    adding new user to db

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ const stateDB = (db) => {
     return getCollection(db, 'state');
 }
 
+const usersDB = (db) => {
+    return getCollection(db, 'users');
+}
+
 const decksDB = (db) => {
     return getCollection(db, 'decks');
 }
@@ -53,11 +57,6 @@ db.connect(DB_URL, (err) => {
 
 app.get('/',
     (req, res) => {
-        connectionsDB(db)
-            .insert({ message: "A user connected" }, (err) => {
-            if (err) console.log('something went wrong adding to the db');
-            console.log('a user connected');
-        })
         res.send("You've reached the server for d1");
 });
 
@@ -75,11 +74,19 @@ app.get('/api/hello',
 
 app.get('/api/decks',
 (req, res) => {
-    console.log('I recieved a get request');
+    console.log('GET /api/decks -------------------------- ');
 
     decksDB(db).find({}).toArray((err, decks) => {
         questionsDB(db).find({}).toArray((err, questions) => {
                 answersDB(db).find({}).toArray((err, answers) => {
+
+                    console.log('im giving back this: ')
+                    console.log('decks: ', decks.length)
+                    console.log('questions: ', questions.length)
+                    console.log('answers: ', answers.length)
+
+                    console.log('SUCCESS -------------------------- ');
+
                     res.json({
                             decks: decks,
                             questions: questions,
@@ -102,6 +109,23 @@ app.post('/api/decks',
                 res.json({"result": "success"})
             })
         })
+    })
+});
+
+
+// INSERT deck
+app.get('/api/users',
+(req, res) => {
+    res.send('GET /api/users')
+});
+
+// INSERT deck
+app.post('/api/users',
+(req, res) => {
+    console.log('POST: /api/users')
+    console.log(req.body);
+    usersDB(db).insert(req.body, (error) => {
+        res.json({"result": "success"})
     })
 });
 
